7b08a322b35d83fcd8c7a299862092493c04ad61
n0tx <rcandra91@msn.com>
2024-09-28 07:59:31 +0700

    Initialize commit Claim Report

---
+++ src/main/java/com/riki/claim_report/AplikasiLaporanKlaimApplication.java
@@ -0,0 +1,13 @@
+package com.riki.claim_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiLaporanKlaimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiLaporanKlaimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riki/claim_report/AplikasiLaporanKlaimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riki.claim_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikasiLaporanKlaimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
